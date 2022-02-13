cc91f94567d7f2238d49c729dd4e47881c0c7919
MacSergey <macsergey@me.com>
2022-02-13 10:30:04 +0200

    Redesigned harmony is removed from allowed dependencies list

--- NoBigTruck/Properties/AssemblyInfo.cs
+++ NoBigTruck/Properties/AssemblyInfo.cs
@@ -16,9 +16,9 @@ using System.Runtime.InteropServices;
 [assembly: Guid("23581242-6240-4851-9a0c-bf93e2a4f4b0")]
 
 #if BETA
-[assembly: AssemblyVersion("1.2.0.27")]
-[assembly: AssemblyFileVersion("1.2.0.27")]
+[assembly: AssemblyVersion("1.2.0.28")]
+[assembly: AssemblyFileVersion("1.2.0.28")]
 #else
-[assembly: AssemblyVersion("1.2.4.1")]
-[assembly: AssemblyFileVersion("1.2.4.1")]
+[assembly: AssemblyVersion("1.2.4.2")]
+[assembly: AssemblyFileVersion("1.2.4.2")]
 #endif
