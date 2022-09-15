68aecd17b0e5a8a7b233600984ce609fb9fccdbb
MacSergey <macsergey@me.com>
2022-09-15 21:03:17 +0300

    Update required game version Update common

--- NoBigTruck/Mod.cs
+++ NoBigTruck/Mod.cs
@@ -34,7 +34,7 @@ namespace NoBigTruck
             new ModVersion(new Version("1.0"), new DateTime(2020, 6, 19)),
         };
 
-        protected override Version RequiredGameVersion => new Version(1, 15, 0, 5);
+        protected override Version RequiredGameVersion => new Version(1, 15, 0, 7);
 
 #if BETA
         public override bool IsBeta => true;
