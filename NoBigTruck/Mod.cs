933424828af5b1ea1a248a4466d87bef4107240b
MacSergey <macsergey@me.com>
2023-06-13 22:21:42 +0300

    - Update for the game version 1.17.1-f2

--- NoBigTruck/Mod.cs
+++ NoBigTruck/Mod.cs
@@ -34,7 +34,7 @@ namespace NoBigTruck
             new ModVersion(new Version("1.0"), new DateTime(2020, 6, 19)),
         };
 
-        protected override Version RequiredGameVersion => new Version(1, 17, 0, 3);
+        protected override Version RequiredGameVersion => new Version(1, 17, 1, 2);
 
 #if BETA
         public override bool IsBeta => true;
