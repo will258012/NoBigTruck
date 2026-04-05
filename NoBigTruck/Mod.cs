a90e00793420020a41a0f1e00ce420cbe88e9f51
Will <125041335+will258012@users.noreply.github.com>
2026-04-05 09:44:38 +0800

    Bump up mod version; add changelog

--- NoBigTruck/Mod.cs
+++ NoBigTruck/Mod.cs
@@ -23,6 +23,7 @@ namespace NoBigTruck
 
         public override List<ModVersion> Versions { get; } = new List<ModVersion>
         {
+            new ModVersion(new Version("1.3.1"), new DateTime(2026, 4, 5)),
             new ModVersion(new Version("1.3"), new DateTime(2022, 9, 14)),
             new ModVersion(new Version("1.2.4"), new DateTime(2021, 8, 22)),
             new ModVersion(new Version("1.2.3"), new DateTime(2021, 8, 7)),
