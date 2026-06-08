7d3e1f7b8b6ca26983acb7b1b05584e64f62053e
Will <125041335+will258012@users.noreply.github.com>
2026-06-08 12:17:22 +0800

    Fix maxWidth setting

--- NoBigTruck/Settings.cs
+++ NoBigTruck/Settings.cs
@@ -170,7 +170,7 @@ namespace NoBigTruck
 
             var useSize = AddUseSizeProperty(parent);
             var maxLength = AddSizeProperty(parent, MaxLength, Localize.MaxLength);
-            var maxWidth = AddSizeProperty(parent, MaxLength, Localize.MaxWidth);
+            var maxWidth = AddSizeProperty(parent, MaxWidth, Localize.MaxWidth);
 
             components.Add(useSize);
             components.Add(maxLength);
